0014479f336fc9a3e08753c10a33ecacfcd39ab2
Edouard <edouard.joret@devinci.Fr>
2020-05-15 21:53:00 +0200

    feat(questions): save question 1 to 20

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 // Q1. Create a variable called `denzel` - that can't be redeclared - and assign it the string 'please denzel, help me'
+const denzel = "please denzel, help me";
 
 // Q2. Use the variable `actor` to create a new variable called `inspirational` that is assigned the string "Is Denzel Washington an inspirational actor?"
 const actor = 'Denzel Washington';
+var inspirational= "Is"+ actor + " an inspirational actor?";
 
 // Q3  Create an object called `family` and give it the following properties:
 // family has been `married` on "1983" (Number)
 // family is not `presidentOfTheUnitedStates` (boolean)
 // family has `children` called "John David", "Katia", "Olivia" and "Malcolm" (array of strings)
+var family={'Married':1983,'presidentOfUnitedStates':false,'Children':['John David','Katia','Olivia','Malcom']};
 
 // Q4. Change
 // * the `name` stage to "A Raisin in the Sun"
@@ -17,10 +20,16 @@ const stage = {
   'year': '2010'
 };
 
+stage.name="A Raisin in the Sun";
+stage.year=2014;
+
 // Q5. Change the `movie` "glory" to "Training Day" using square bracket notation
 const awards = {
   'movie': 'glory'
 };
+
+awards['movie']="Training Day";
+
 
 // Q6. Create an object called "earlywork"
 // The key should be the movie name and the value the release year
@@ -31,42 +40,51 @@ const awards = {
 // * Cry Freedom (1987)
 // * For Queen and Countryy (1988)
 
+var earlywork = { "Carbon Copy":"1981","A Soldier Story":"1984","Power":"1986","Cry Freedom":"1987","For Queen and Countryy":"1988"};
+
 // Q7. Return a new array from `debaters` with all item in uppercase
 const debaters = ['we do', 'what we have to do', 'in order to do', 'what we want to do'];
+const newdebaters=[];
+for(let i=0;i<debaters.length;i++)
+{newdebaters.push(debaters[i].toUpperCase());}
 
 // Q8. Using this array.
 // Do the following 5 steps
 const directors = ['spikelee', 'ridleyscott', 'zemeckis'];
 
 // 1. add "tonyscott" value to the end of `directors` array
-
+directors.push("tonyscott");
 // 2. remove "spikelee" value and store it in a variable called firstDirector
-
+var firstDirector=directors.shift();
 // 3. add "himself" value to the start of `directors` array
-
+directors.unshift("himself");
 // 4. remove "ridleyscott" value from the array and store it in a variable called secondDirector
-
+var secondDirector=directors.splice(1,1);
 // 5. let 'zemeckis' in the array but put a copy of it on a variable called thirdDirector
-
+var thirdDirector=directors[2];
 
 // Q9. Write the function `duplicate` that return the expected result
 console.log(duplicate(['Happily', 'Ever', 'After', 'Fairy', 'Tales', 'for', 'Every', 'Child']));
 // ❯ ['Happily', 'Ever', 'After', 'Fairy', 'Tales', 'for', 'Every', 'Child', 'Happily', 'Ever', 'After', 'Fairy', 'Tales', 'for', 'Every', 'Child']
-
+function duplicate(array){
+const length=array.length;
+for (let i=0;i<length;i++){
+	array.push(array[i]);
+	}
+	return array;
+}
 
 // Q10. Refactor the current ES5-style function `police` in ES6-style.
-const police = function (names) {
+const police = (names) => {
   const results = [];
 
-  for (let i = 0; i < names.length; i++) {
-    results.push(names[i] + 'I am the police');
-  }
+  names.forEach(name=> results.push('${name} I am the police'));
   return results;
 };
 
 const quotes = ['You will never see the light of... ', 'I run shit around here ', 'King Kong aint got shit on me '];
 
-console.log(police(chorus));
+console.log(police(quotes));
  // ❯ ['You will never see the light of... I am the police', 'I run shit around here I am the police', 'King Kong aint got shit on me I am the police'];
 
 
@@ -75,10 +93,12 @@ console.log(police(chorus));
 
 // Q12. Related to the https://www.tvmaze.com/people/66167/denzel-washington link
 // How many http requests are performed to render the page?
+// There are 20 requests
 
 // Q13. Related to the following api call with "curl"
 // ❯ curl "http://api.tvmaze.com/people/6616"
 // Could you describe and explain the response?
+//We've got as a response a json document which describe someone called Zach Shirey, and we can see 2 images and one link and some empty values. moreover, its a GET request.
 
 // Q14. Refactor the following codebase with a promise notation
 fs.readFile(filePath, function(err, data) {
@@ -108,11 +128,16 @@ fetch('http://api.tvmaze.com/search/people?q=denzel+washington')
   .then(console.log);
 
 // Q16. Give me at least 3 memorable websites that engage to continue (because of nice UX/UI AND avoid to give me facebook, airbnb etc...)
+//Twitch, W3schools, Paypal
 
 // Q17. Describe an ESILV project that you worked on that you’re proud of?
+//It was my 3th year project, which aim was to facilitate the studies of students leaving abroad. So we made Mobile app and Web app to make this.
 
 // Q18. Describe an ESILV project that you worked on that you’re not so proud of?
+//It was my last year project when i was in financial major, i didn't really like the subject so i didn't put all my energy in it. That's why i'm not proud of it and decided to change major to go in IBO.
 
 // Q19. What are some things you like about the developer tools you use?
+//I like developer tools i use because it facilitate me to understand my errors and to correct it. It 
 
 // Q20. Last question: could you explain me - in your terms - why the title of the course is "Web Application Architecture(s)" and not "Web Application Architecture"?
+//Because there's not only one architecture to make a Web application. 
